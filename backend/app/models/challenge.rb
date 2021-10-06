ea56c2c8ed24095bae19c428d5f662f36f799698
ming-00 <e0470728@u.nus.edu>
2021-10-07 01:05:51 +0800

    Appease rubocop overlord

--- backend/app/models/challenge.rb
+++ backend/app/models/challenge.rb
@@ -1,11 +1,13 @@
-class Challenge < ApplicationRecord
-    belongs_to :user
-    belongs_to :category
-    has_many :tasks, dependent: :destroy
+# frozen_string_literal: true
 
-    default_scope -> { order(name: :asc) }
-    validates :name,
-        :presence => {:message => " can't be blank."},
-        length: {minimum: 1, maximum: 25, 
-            :message => " must be between 1 and 25 characters."}
+class Challenge < ApplicationRecord
+  belongs_to :user
+  belongs_to :category
+  has_many :tasks, dependent: :destroy
+
+  default_scope -> { order(name: :asc) }
+  validates :name,
+            presence: { message: " can't be blank." },
+            length: { minimum: 1, maximum: 25,
+                      message: ' must be between 1 and 25 characters.' }
 end
