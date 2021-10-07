4f12a63e62843039ab717cf8f8760852bec0b89f
ming-00 <e0470728@u.nus.edu>
2021-10-07 22:47:44 +0800

    Add tasks controller

--- backend/app/controllers/categories_controller.rb
+++ backend/app/controllers/categories_controller.rb
@@ -1,26 +1,23 @@
 # frozen_string_literal: true
 
 class CategoriesController < ApplicationController
-    before_action :set_category, only: %i[show update destroy]
-  
-    def index
-      @categories = Category.all
-    end
-  
-    def show; 
-        @category = Category.find(params.require(:id))
-    end
-  
-    private
-  
-    # Use callbacks to share common setup or constraints between actions.
-    def set_category
-      @category = Category.find(params[:id])
-    end
-  
-    # Only allow a list of trusted parameters through.
-    def category_params
-      params.require(:category).permit(:title)
-    end
+  def index
+    @categories = Category.all
   end
-  
+
+  def show
+    @category = Category.find(params.require(:id))
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def category_params
+    params.require(:category).permit(:title)
+  end
+end
---
+++ backend/app/controllers/tasks_controller.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+class TasksController < ApplicationController
+  def index
+    @tasks = Tasks.all
+  end
+
+  def show
+    @task = Task.find(params.require(:id))
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save!
+
+    render 'tasks/show', status: :created
+  end
+
+  def update
+    @task = Task.find(params.require(:id))
+    @task.update!(task_params)
+
+    render 'tasks/show', status: :ok
+  end
+
+  def destroy
+    @task = Task.find(params.require(:id))
+    @task.destroy!
+
+    render 'layouts/empty', status: :no_content
+  end
+
+  private
+
+  def task_params
+    params.require(:data).permit(:challenge_id, :name, :description, :index)
+  end
+
+  rescue_from ActiveRecord::RecordNotFound do |e|
+    @errors = e
+
+    show_error_message('Task cannot be found')
+    render 'layouts/empty', status: :not_found
+  end
+end
