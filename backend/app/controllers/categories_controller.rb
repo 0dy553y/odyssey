165cce7888efc6ee37d7838fbd60626be834a423
ming-00 <e0470728@u.nus.edu>
2021-10-08 00:46:41 +0800

    Remove extra stuff from categories controller

--- backend/app/controllers/categories_controller.rb
+++ backend/app/controllers/categories_controller.rb
@@ -8,16 +8,4 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params.require(:id))
   end
-
-  private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_category
-    @category = Category.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
-  def category_params
-    params.require(:category).permit(:title)
-  end
 end
