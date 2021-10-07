ca974c79a6ccb8f0049ee1d0fdc8bbf534ec01bf
Sebastian Toh <sebas.tsj.98@gmail.com>
2021-10-07 18:30:44 +0800

    Add login page

---
+++ frontend/src/pages/login/LoginPage.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+const LoginPage: React.FC = () => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    console.log('LOG IN', event);
+  };
+
+  return (
+    <Box
+      sx={{
+        marginTop: 8,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <Typography component="h1" variant="h5">
+        Login
+      </Typography>
+      <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="username"
+          label="Username"
+          name="username"
+          autoComplete="username"
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Log In
+        </Button>
+        <Grid item>
+          <Link href="#" variant="body2">
+            {'No account yet? Register'}
+          </Link>
+        </Grid>
+      </Box>
+    </Box>
+  );
+};
+export default LoginPage;
