78beee6fee6144b999e98003e30fd30e499428a5
ming-00 <e0470728@u.nus.edu>
2021-10-07 22:18:00 +0800

    Add constraints for challenge and task

--- backend/app/models/task.rb
+++ backend/app/models/task.rb
@@ -2,4 +2,10 @@
 
 class Task < ApplicationRecord
   belongs_to :challenge
+
+  default_scope -> { order(index: :asc) }
+  validates :name,
+            presence: { message: " can't be blank." },
+            length: { minimum: 1, maximum: 40,
+                      message: ' must be between 1 and 40 characters.' }
 end
