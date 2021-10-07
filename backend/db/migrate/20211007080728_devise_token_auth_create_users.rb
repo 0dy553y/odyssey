5bbe4b855d0944f0af9d40708cdff76fcc9f80bf
Sebastian Toh <sebas.tsj.98@gmail.com>
2021-10-07 20:30:44 +0800

    Fix all lint errors

--- backend/db/migrate/20211007080728_devise_token_auth_create_users.rb
+++ backend/db/migrate/20211007080728_devise_token_auth_create_users.rb
@@ -3,8 +3,8 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.1]
     
     create_table(:users) do |t|
       ## Required
-      t.string :provider, null: false, default: "username"
-      t.string :uid, null: false, default: ""
+      t.string :provider, null: false, default: 'username'
+      t.string :uid, null: false, default: ''
 
       ## Database authenticatable
       t.string :encrypted_password, null: false
@@ -12,7 +12,7 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.1]
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
-      t.boolean  :allow_password_change, :default => false
+      t.boolean  :allow_password_change, default: false
 
       ## Rememberable
       t.datetime :remember_created_at
@@ -34,9 +34,9 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.1]
       t.timestamps
     end
 
-    add_index :users, :username,                unique: true
+    add_index :users, :username,             unique: true
     add_index :users, :email,                unique: true
-    add_index :users, [:uid, :provider],     unique: true
+    add_index :users, %i[uid provider],      unique: true
     add_index :users, :reset_password_token, unique: true
     add_index :users, :confirmation_token,   unique: true
   end
