5501f954bab17834553c6f154b3700047036ea2b
ming-00 <e0470728@u.nus.edu>
2021-10-08 12:13:18 +0800

    Fix typo

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,9 +1,11 @@
 import CategoriesAPI from './categories';
 import ChallengesAPI from './challenges';
+import TasksAPI from './tasks';
 
 const api = Object.freeze({
   challenges: new ChallengesAPI(),
   categories: new CategoriesAPI(),
+  tasks: new TasksAPI(),
 });
 
 export default api;
