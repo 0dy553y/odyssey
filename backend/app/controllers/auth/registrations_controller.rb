50d2ffd220e03d68c09e0472cd24ae8d31abcde4
Sebastian Toh <sebas.tsj.98@gmail.com>
2021-10-08 20:15:50 +0800

    Fix lint errors

--- backend/app/controllers/auth/registrations_controller.rb
+++ backend/app/controllers/auth/registrations_controller.rb
@@ -1,21 +1,23 @@
 # frozen_string_literal: true
 
-module Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
-  def render_create_success
-    render json: {
-      status: 'success',
-      payload: { data: resource_data },
-      messages: [],
-      errors: []
-    }
-  end
+module Auth
+  class RegistrationsController < DeviseTokenAuth::RegistrationsController
+    def render_create_success
+      render json: {
+        status: 'success',
+        payload: { data: resource_data },
+        messages: [],
+        errors: []
+      }
+    end
 
-  def render_create_error
-    render json: {
-      status: 'error',
-      payload: { data: resource_data },
-      messages: [],
-      errors: resource_errors
-    }, status: :unprocessable_entity
+    def render_create_error
+      render json: {
+        status: 'error',
+        payload: { data: resource_data },
+        messages: [],
+        errors: resource_errors
+      }, status: :unprocessable_entity
+    end
   end
 end
--- backend/app/controllers/auth/sessions_controller.rb
+++ backend/app/controllers/auth/sessions_controller.rb
@@ -1,19 +1,21 @@
 # frozen_string_literal: true
 
-module Auth::SessionsController < DeviseTokenAuth::SessionsController
-  def render_create_success
-    render json: {
-      payload: { data: resource_data(resource_json: @resource.token_validation_response) },
-      messages: [],
-      errors: []
-    }
-  end
+module Auth
+  class SessionsController < DeviseTokenAuth::SessionsController
+    def render_create_success
+      render json: {
+        payload: { data: resource_data(resource_json: @resource.token_validation_response) },
+        messages: [],
+        errors: []
+      }
+    end
 
-  def render_create_error_bad_credentials
-    render json: {
-      payload: { data: nil },
-      messages: [],
-      errors: [I18n.t('devise_token_auth.sessions.bad_credentials')]
-    }, status: :unauthorized
+    def render_create_error_bad_credentials
+      render json: {
+        payload: { data: nil },
+        messages: [],
+        errors: [I18n.t('devise_token_auth.sessions.bad_credentials')]
+      }, status: :unauthorized
+    end
   end
 end
