7730dbbcdfd8cda77e436063a7aeb147602475f4
ming-00 <e0470728@u.nus.edu>
2021-10-08 13:00:52 +0800

    Add resource nesting for tasks backend

--- backend/app/controllers/tasks_controller.rb
+++ backend/app/controllers/tasks_controller.rb
@@ -1,36 +1,44 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
+  before_action :find_challenge
+  before_action :set_task, only: [:show, :update, :destroy]
+
   def index
-    @tasks = Task.all
+    @tasks = @challenge.tasks
   end
 
   def show
-    @task = Task.find(params.require(:id))
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = @challenge.tasks.build(task_params)
     @task.save!
 
     render 'tasks/show', status: :created
   end
 
   def update
-    @task = Task.find(params.require(:id))
     @task.update!(task_params)
 
     render 'tasks/show', status: :ok
   end
 
   def destroy
-    @task = Task.find(params.require(:id))
     @task.destroy!
 
     render 'layouts/empty', status: :no_content
   end
 
   private
+
+  def find_challenge
+    @challenge = Challenge.find(params[:challenge_id])
+  end
+
+  def set_task
+    @task = @challenge.tasks.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:challenge_id, :name, :description, :index)
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :categories
-  resources :challenges
-  resources :tasks
+  resources :challenges do
+    resources :tasks
+  end
 end
