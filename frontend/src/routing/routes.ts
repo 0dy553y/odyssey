6ab7fc2d23128041fa398812b6cc6d10a0baee12
Sebastian Toh <sebas.tsj.98@gmail.com>
2021-10-09 13:01:24 +0800

    Add profile route

--- frontend/src/routing/routes.ts
+++ frontend/src/routing/routes.ts
@@ -1,13 +1,16 @@
 import HomePage from '../pages/home';
 import LoginPage from '../pages/login';
+import ProfilePage from '../pages/profile';
 import RegistrationPage from '../pages/registration';
 
 export const HOME_ROUTE = '/';
 export const LOGIN_ROUTE = '/login';
 export const REGISTER_ROUTE = '/register';
+export const PROFILE_ROUTE = '/profile';
 
 export const routes = [
   { path: LOGIN_ROUTE, component: LoginPage, isPublic: true },
   { path: REGISTER_ROUTE, component: RegistrationPage, isPublic: true },
+  { path: PROFILE_ROUTE, component: ProfilePage },
   { path: HOME_ROUTE, component: HomePage },
 ];
