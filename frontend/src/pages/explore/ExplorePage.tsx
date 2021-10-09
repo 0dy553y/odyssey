4e27a226c18525fdbe4bd8177e26d3387a3eb7fc
ming-00 <e0470728@u.nus.edu>
2021-10-09 17:24:49 +0800

    Add search bar finally

--- frontend/src/pages/explore/ExplorePage.tsx
+++ frontend/src/pages/explore/ExplorePage.tsx
@@ -1,7 +1,55 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import InputBase from '@mui/material/InputBase';
+import { styled, alpha } from '@mui/material/styles';
+import { ReactComponent as SearchIcon } from '../../assets/icons/search-icon.svg';
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  //   borderRadius: theme.shape.borderRadius,
+  borderRadius: 40,
+  'margin-top': 20,
+  backgroundColor: alpha(theme.palette.common.black, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.black, 0.25),
+  },
+  width: '100%',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  width: '70%',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1.5, 0, 1.5, 0),
+    // vertical padding + font size from searchIcon
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
 
 const ExplorePage: React.FC = () => {
-  return <div>Werucome</div>;
+  return (
+    <Box>
+      Find your next challenge
+      <Search>
+        <SearchIconWrapper>
+          <SearchIcon />
+        </SearchIconWrapper>
+        <StyledInputBase
+          placeholder="Search…"
+          inputProps={{ 'aria-label': 'search' }}
+        />
+      </Search>
+    </Box>
+  );
 };
 
 export default ExplorePage;
