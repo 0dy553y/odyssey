749a4ee56d6a38295fbf415d27827e5b13f79468
Sebastian Toh <sebas.tsj.98@gmail.com>
2021-10-09 13:18:31 +0800

    Add token validation

---
+++ backend/app/controllers/auth/token_validations_controller.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Auth
+  class TokenValidationsController < DeviseTokenAuth::TokenValidationsController
+    include AuthHelper
+
+    private
+
+    def render_validate_token_success
+      show_success_message("Welcome back, #{@resource.username}!")
+      render 'auth/user', status: :created
+    end
+
+    def render_validate_token_error
+      show_error_message("Token is not valid!")
+      render 'layouts/empty', status: :unauthorized
+    end
+  end
+end
