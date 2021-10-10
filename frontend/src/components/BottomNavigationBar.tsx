f68e03635436a6ae0129787e36e458567a72567c
Sebastian Toh <sebas.tsj.98@gmail.com>
2021-10-10 09:45:42 +0800

    Prevent nav bar from hiding content

--- frontend/src/components/BottomNavigationBar.tsx
+++ frontend/src/components/BottomNavigationBar.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { BottomNavigation, BottomNavigationAction, Paper } from '@mui/material';
+import {
+  AppBar,
+  BottomNavigation,
+  BottomNavigationAction,
+  Paper,
+  Toolbar,
+} from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
@@ -20,6 +26,7 @@ const useStyles = makeStyles(() => ({
     bottom: 0,
     left: 0,
     right: 0,
+    marginTop: '50px',
   },
 }));
 
@@ -28,41 +35,48 @@ const BottomNavigationBar: React.FC = () => {
   const location = useLocation();
 
   return (
-    <Paper className={classes.paper} elevation={3}>
-      <BottomNavigation
-        showLabels
-        value={getFirstPathSegment(location.pathname)}
-      >
-        <BottomNavigationAction
-          label="Home"
-          icon={<HomeOutlinedIcon />}
-          value={HOME_ROUTE}
-          component={Link}
-          to={HOME_ROUTE}
-        />
-        <BottomNavigationAction
-          label="Explore"
-          icon={<SearchOutlinedIcon />}
-          value={EXPLORE_ROUTE}
-          component={Link}
-          to={EXPLORE_ROUTE}
-        />
-        <BottomNavigationAction
-          label="Feed"
-          icon={<PeopleOutlineIcon />}
-          value={FEED_ROUTE}
-          component={Link}
-          to={FEED_ROUTE}
-        />
-        <BottomNavigationAction
-          label="Profile"
-          icon={<AccountCircleOutlinedIcon />}
-          value={PROFILE_ROUTE}
-          component={Link}
-          to={PROFILE_ROUTE}
-        />
-      </BottomNavigation>
-    </Paper>
+    <>
+      {/*
+      This extra toolbar is so that the navigation bar does not hide content
+      See: https://mui.com/components/app-bar/#fixed-placement
+      */}
+      <Toolbar />
+      <Paper className={classes.paper} elevation={3}>
+        <BottomNavigation
+          showLabels
+          value={getFirstPathSegment(location.pathname)}
+        >
+          <BottomNavigationAction
+            label="Home"
+            icon={<HomeOutlinedIcon />}
+            value={HOME_ROUTE}
+            component={Link}
+            to={HOME_ROUTE}
+          />
+          <BottomNavigationAction
+            label="Explore"
+            icon={<SearchOutlinedIcon />}
+            value={EXPLORE_ROUTE}
+            component={Link}
+            to={EXPLORE_ROUTE}
+          />
+          <BottomNavigationAction
+            label="Feed"
+            icon={<PeopleOutlineIcon />}
+            value={FEED_ROUTE}
+            component={Link}
+            to={FEED_ROUTE}
+          />
+          <BottomNavigationAction
+            label="Profile"
+            icon={<AccountCircleOutlinedIcon />}
+            value={PROFILE_ROUTE}
+            component={Link}
+            to={PROFILE_ROUTE}
+          />
+        </BottomNavigation>
+      </Paper>
+    </>
   );
 };
 
