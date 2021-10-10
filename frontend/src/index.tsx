bb7c95c324d9649e752d40b16419fe07c31f1cf7
Sebastian Toh <sebas.tsj.98@gmail.com>
2021-10-10 10:30:28 +0800

    Add ThemeProvider

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -7,15 +7,19 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
 
 const store = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
