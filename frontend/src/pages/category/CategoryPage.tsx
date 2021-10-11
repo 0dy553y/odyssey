4af9e5281e23f7fd0c6d092c9db094a92ffd7925
ming-00 <e0470728@u.nus.edu>
2021-10-11 20:14:13 +0800

    Fix duplicate problemos

--- frontend/src/pages/category/CategoryPage.tsx
+++ frontend/src/pages/category/CategoryPage.tsx
@@ -2,8 +2,6 @@ import React, { useEffect } from 'react';
 import Box from '@mui/material/Box';
 import CategoryHeader from '../../components/category/CategoryHeader';
 import CategoryListItem from '../../components/category/CategoryListItem';
-import { useDispatch } from 'react-redux';
-import { loadCategory } from 'store/categories/operations';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { RootState } from 'store';
@@ -12,11 +10,6 @@ import { getHeadingFromCategory } from 'utils/naming';
 
 const ExplorePage: React.FC = () => {
   const { categoryId } = useParams<{ categoryId: string }>();
-
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(loadCategory(Number(categoryId)));
-  }, []);
 
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   const category = useSelector((state: RootState) =>
