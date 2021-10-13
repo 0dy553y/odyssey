32ad960506512f0a311cf71fd6c74c5514e4c6ba
ming-00 <e0470728@u.nus.edu>
2021-10-13 14:46:03 +0800

    Add fonts

--- frontend/src/utils/theme.ts
+++ frontend/src/utils/theme.ts
@@ -1,6 +1,9 @@
 import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
+  typography: {
+    fontFamily: `"CircularStd", "Helvetica", "Arial", sans-serif`,
+  },
   components: {
     MuiAppBar: {
       styleOverrides: {
