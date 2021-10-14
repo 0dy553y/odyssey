a3b110ed61a5a9ed4c017e194b461dd96b494dd9
jialin7878 <E0425498@u.nus.edu>
2021-10-14 23:54:08 +0800

    Add route to get user tasks for challenge

--- backend/app/controllers/user_tasks_controller.rb
+++ backend/app/controllers/user_tasks_controller.rb
@@ -4,4 +4,15 @@ class UserTasksController < ApplicationController
   def tasks_for_day
     @user_tasks = current_user.user_tasks.where(scheduled_for: params.require(:date).to_date.all_day)
   end
+
+  def tasks_for_challenge
+    # looks for ongoing attempt
+    @user_challenge = current_user.user_challenges.find_by(challenge_id: params.require(:challenge_id), completed_at: nil, forfeited_at: nil)
+    if @user_challenge != nil
+      p @user_challenge.id
+      @user_tasks = current_user.user_tasks.where(user_challenge_id: @user_challenge.id)
+    end
+    p @user_tasks.inspect
+    render 'user_tasks/tasks_for_day', status: :ok
+  end
 end
