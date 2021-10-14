e3ede6747011331ac5cb7cef3484b4f00ea78b70
Sebastian Toh <sebas.tsj.98@gmail.com>
2021-10-14 12:36:32 +0800

    Redirect authenticated user if access log in / register page

--- frontend/src/routing/routes.ts
+++ frontend/src/routing/routes.ts
@@ -1,12 +1,12 @@
 import EditProfilePage from 'pages/editProfile';
 import FriendsPage from 'pages/friends/FriendsPage';
 import CategoryPage from '../pages/category';
+import ChallengeDetailsPage from '../pages/challenge';
 import ExplorePage from '../pages/explore';
 import HomePage from '../pages/home';
 import LoginPage from '../pages/login';
 import ProfilePage from '../pages/profile';
 import RegistrationPage from '../pages/registration';
-import ChallengeDetailsPage from '../pages/challenge';
 import { RouteEntry } from '../types/routes';
 
 export const HOME_ROUTE = '/';
@@ -19,10 +19,14 @@ export const CATEGORY_ROUTE = '/category';
 export const EDIT_PROFILE_ROUTE = '/profile/edit';
 export const FRIENDS_ROUTE = '/friends';
 
-export const publicRoutes: RouteEntry[] = [
+// Routes that are accessible only if user is not authenticated
+export const notAuthenticatedRoutes: RouteEntry[] = [
   { path: LOGIN_ROUTE, component: LoginPage },
   { path: REGISTER_ROUTE, component: RegistrationPage },
 ];
+
+// Public routes that are accessible regardless of authentication status
+export const publicRoutes: RouteEntry[] = [];
 
 export const privateRoutes: RouteEntry[] = [
   { path: EDIT_PROFILE_ROUTE, component: EditProfilePage, exact: true },
