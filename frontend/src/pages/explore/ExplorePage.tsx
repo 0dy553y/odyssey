e756b5c78424c170b8cd8fd26a090919967cf49b
ming-00 <e0470728@u.nus.edu>
2021-10-15 01:25:40 +0800

    Implement search feature for challenges

--- frontend/src/pages/explore/ExplorePage.tsx
+++ frontend/src/pages/explore/ExplorePage.tsx
@@ -1,25 +1,38 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import Searchbar from '../../components/common/Searchbar';
 import CategoryPreview from '../../components/explore/CategoryPreview';
 import Typography from '@mui/material/Typography';
 import { getCategoryList } from 'store/categories/selectors';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from 'store';
 import { Link } from 'react-router-dom';
 import { getHeadingFromCategory } from 'utils/naming';
 import { CATEGORY_ROUTE } from '../../routing/routes';
+import CategoryListItem from '../../components/category/CategoryListItem';
+import { loadAllChallenges } from 'store/challenges/operations';
+import { getChallengeList } from 'store/challenges/selectors';
+
 import './ExplorePage.scss';
 
 const ExplorePage: React.FC = () => {
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   const categories = useSelector((state: RootState) => getCategoryList(state))!;
+  const dispatch = useDispatch();
   const [value, setValue] = useState('');
 
   const eventhandler = (data: string) => {
-    console.log(data);
     setValue(data);
   };
+
+  useEffect(() => {
+    dispatch(loadAllChallenges());
+  }, []);
+
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const challenges = useSelector((state: RootState) =>
+    getChallengeList(state)
+  )!;
 
   return (
     <Box sx={{ paddingTop: '2em' }}>
@@ -28,7 +41,30 @@ const ExplorePage: React.FC = () => {
         placeholder="Search by challenge name..."
         func={eventhandler}
       />
-      {value.length > 0 && <h2>You have unread messages.</h2>}
+      {value.length > 0 && (
+        <ul>
+          {challenges
+            .filter((challenge) =>
+              challenge.name.toLowerCase().includes(value.toLowerCase())
+            )
+            .map((challenge) => (
+              <li key={challenge.id}>
+                <Link
+                  to={{
+                    pathname: `${CATEGORY_ROUTE}/${challenge.categoryId}/${challenge.id}`,
+                    state: { challenge: challenge },
+                  }}
+                  style={{ textDecoration: 'none' }}
+                >
+                  <CategoryListItem
+                    name={challenge.name}
+                    duration={challenge.duration}
+                  />
+                </Link>
+              </li>
+            ))}
+        </ul>
+      )}
       {value.length == 0 && (
         <>
           <Typography variant="h6" sx={{ fontStyle: 'italic' }}>
