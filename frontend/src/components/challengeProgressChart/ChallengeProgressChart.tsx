547a43b30b7e5ed98c5fc5661c991e39687a850c
Sebastian Toh <sebas.tsj.98@gmail.com>
2021-10-14 19:21:23 +0800

    Replace chart with mock data

--- frontend/src/components/challengeProgressChart/ChallengeProgressChart.tsx
+++ frontend/src/components/challengeProgressChart/ChallengeProgressChart.tsx
@@ -8,67 +8,56 @@ import {
   Tooltip,
   ResponsiveContainer,
 } from 'recharts';
+import { displayDate } from 'utils/formatting';
+
+import './ChallengeProgressChart.scss';
 
 interface ChallengeProgressChartProps {
   height: string | number;
   color?: string;
+  data: ChallengeProgressData[];
+  totalNumberOfTasks: number;
 }
 
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+interface ChallengeProgressData {
+  taskCompletionDate: Date;
+  taskIndex: number;
+}
+
+interface ProgressChartDataPoint extends ChallengeProgressData {
+  percentage: number;
+  timestamp: number;
+}
+
+const getDataPoints = (
+  data: ChallengeProgressData[],
+  totalNumberOfTasks: number
+): ProgressChartDataPoint[] => {
+  return data.map((datum) => {
+    return {
+      ...datum,
+      percentage: ((datum.taskIndex + 1) / totalNumberOfTasks) * 100,
+      timestamp: datum.taskCompletionDate.getTime(),
+    };
+  });
+};
 
 const ChallengeProgressChart: React.FC<ChallengeProgressChartProps> = ({
   height,
   color,
+  data,
+  totalNumberOfTasks,
 }) => {
+  const isChallengeCompleted = data.length === totalNumberOfTasks;
+
   return (
-    <ResponsiveContainer width="100%" height={height}>
+    <ResponsiveContainer
+      width="100%"
+      height={height}
+      className={'progress-chart-container'}
+    >
       <AreaChart
-        width={500}
-        height={400}
-        data={data}
+        data={getDataPoints(data, totalNumberOfTasks)}
         margin={{
           top: 10,
           right: 30,
@@ -77,7 +66,13 @@ const ChallengeProgressChart: React.FC<ChallengeProgressChartProps> = ({
         }}
       >
         <defs>
-          <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+          <linearGradient
+            id="progress-chart-area-gradient"
+            x1="0"
+            y1="0"
+            x2="0"
+            y2="1"
+          >
             <stop
               offset="5%"
               stopColor={color ?? '#8884d8'}
@@ -88,15 +83,27 @@ const ChallengeProgressChart: React.FC<ChallengeProgressChartProps> = ({
         </defs>
 
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
+        <XAxis
+          dataKey="timestamp"
+          tickFormatter={(timestamp) => displayDate(new Date(timestamp))}
+          type="number"
+          domain={[
+            'dataMin',
+            (dataMax: Date) => {
+              console.log(isChallengeCompleted, dataMax);
+
+              return isChallengeCompleted ? dataMax : new Date();
+            },
+          ]}
+        />
+        <YAxis domain={[0, 100]} />
         <Tooltip />
         <Area
           type="monotone"
-          dataKey="uv"
+          dataKey="percentage"
           stroke={color ?? '#8884d8'}
           fillOpacity={1}
-          fill="url(#colorUv)"
+          fill="url(#progress-chart-area-gradient)"
         />
       </AreaChart>
     </ResponsiveContainer>
--- frontend/src/pages/challenge/UserChallengeStats.tsx
+++ frontend/src/pages/challenge/UserChallengeStats.tsx
@@ -2,15 +2,30 @@ import React from 'react';
 import ChallengeProgressChart from 'components/challengeProgressChart';
 import { useTheme } from '@mui/styles';
 import { Theme } from '@mui/material';
+import { sub } from 'date-fns';
 
 interface UserChallengeStatsProps {
   TODO?: string;
 }
 
+const mockChallengeProgressChartProps = {
+  data: Array.from(Array(6).keys()).map((i) => {
+    return {
+      taskCompletionDate: sub(sub(new Date(), { days: 5 }), { days: 6 - i }),
+      taskIndex: i,
+    };
+  }),
+  totalNumberOfTasks: 10,
+};
+
 const UserChallengeStats: React.FC<UserChallengeStatsProps> = (props) => {
   const theme: Theme = useTheme();
   return (
-    <ChallengeProgressChart height={220} color={theme.palette.primary.main} />
+    <ChallengeProgressChart
+      height={220}
+      color={theme.palette.primary.main}
+      {...mockChallengeProgressChartProps}
+    />
   );
 };
 
