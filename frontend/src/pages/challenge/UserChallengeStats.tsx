a8c9a472c93c730af67dd9f2f4893e21a7b5cbfb
Sebastian Toh <sebas.tsj.98@gmail.com>
2021-10-15 20:50:49 +0800

    Add recurring day checkboxes

--- frontend/src/pages/challenge/UserChallengeStats.tsx
+++ frontend/src/pages/challenge/UserChallengeStats.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import ChallengeProgressChart from 'components/challengeProgressChart';
 import { useTheme } from '@mui/styles';
-import { Theme } from '@mui/material';
+import {
+  Box,
+  Checkbox,
+  Divider,
+  FormControl,
+  FormControlLabel,
+  Stack,
+  FormGroup,
+  Theme,
+  Typography,
+} from '@mui/material';
 import { add, sub } from 'date-fns';
+import { DayOfWeek } from 'types/date';
+
+import './UserChallengeStats.scss';
 
 interface UserChallengeStatsProps {
   TODO?: string;
@@ -24,11 +37,36 @@ const mockChallengeProgressChartProps = {
 const UserChallengeStats: React.FC<UserChallengeStatsProps> = (props) => {
   const theme: Theme = useTheme();
   return (
-    <ChallengeProgressChart
-      height={220}
-      color={theme.palette.primary.main}
-      {...mockChallengeProgressChartProps}
-    />
+    <Box className="user-challenge-stats">
+      <Typography component="div" variant="h6" sx={{ fontStyle: 'italic' }}>
+        Recurring days
+      </Typography>
+
+      <FormControl component="fieldset" className="day-checkbox-container">
+        <FormGroup>
+          <Stack direction="row" justifyContent="space-between" spacing={0}>
+            {Object.values(DayOfWeek).map((day) => (
+              <FormControlLabel
+                key={day}
+                value={day}
+                control={<Checkbox />}
+                label={day[0]}
+                labelPlacement="top"
+                className={'day-checkbox'}
+              />
+            ))}
+          </Stack>
+        </FormGroup>
+      </FormControl>
+
+      <Divider className="divider" />
+
+      <ChallengeProgressChart
+        height={220}
+        color={theme.palette.primary.main}
+        {...mockChallengeProgressChartProps}
+      />
+    </Box>
   );
 };
 
