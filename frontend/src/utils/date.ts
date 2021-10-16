1677abd24b03b25c5d7cb96d4c0acd3ffacfe6d8
Ian Yong <ianyongyc@gmail.com>
2021-10-16 20:18:55 +0800

    Add utility function for generating all dates in a month

---
+++ frontend/src/utils/date.ts
@@ -0,0 +1,9 @@
+export function getDatesInMonth(month: number, year: number): Date[] {
+  const date = new Date(year, month, 1);
+  const days = [];
+  while (date.getMonth() === month) {
+    days.push(new Date(date));
+    date.setDate(date.getDate() + 1);
+  }
+  return days;
+}
