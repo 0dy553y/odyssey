83a02abdabe2a477b3b1943a35710f2f6aa4b474
jialin7878 <59867654+jialin7878@users.noreply.github.com>
2021-10-16 16:46:16 +0800

    Non functional skeleton code for joining challenge

--- backend/app/controllers/challenges_controller.rb
+++ backend/app/controllers/challenges_controller.rb
@@ -9,6 +9,38 @@ class ChallengesController < ApplicationController
     @challenge = Challenge.find(params.require(:id))
   end
 
+  def join
+    schedule = schedule_params
+    now = DateTime.now
+    challengeId = params.require(:challenge_id)
+    @userChallenge = UserChallenge.new(
+      :user_id => current_user.id,
+      :challenge_id => challengeId,
+      :created_at => now,
+      :updated_at => now,
+    )
+    @userChallenge.save!
+
+    @tasks = Tasks.find(challenge_id: challengeId)
+
+    schedule_date_pointer = now
+    @tasks.each { |task|
+      # find next available time in schedule
+      while not schedule[schedule_date_pointer.strftime('%A')]:
+        schedule_date_pointer += 1
+      
+      @userTask = UserTask.new(
+        :user_id => current_user.id,
+        :challenge_id => challengeId,
+        :task_id => task.id,
+        :scheduled_for => schedule_date_pointer,
+      )
+      @userTask.save!
+      }
+
+    render 'layouts/empty', status: :ok
+  end
+
   def create
     @challenge = Challenge.new(challenge_params)
     @challenge.save!
@@ -34,6 +66,10 @@ class ChallengesController < ApplicationController
 
   def challenge_params
     params.require(:challenge).permit(:category_id, :name, :description, :schedule, :duration)
+  end
+
+  def schedule_params
+    params.require(:schedule).permit(:monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday)
   end
 
   rescue_from ActiveRecord::RecordNotFound do |e|
