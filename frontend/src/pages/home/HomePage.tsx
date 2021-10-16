8f5f1d171c22c9585569de25dc9db69bff181ab9
Ian Yong <ianyongyc@gmail.com>
2021-10-16 19:52:14 +0800

    Add greeting to homepage

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -4,20 +4,29 @@ import { useDispatch, useSelector } from 'react-redux';
 import UserTaskCarousel from '../../components/home/UserTaskCarousel';
 import { getUserTaskListForDay } from '../../store/usertasks/selectors';
 import { RootState } from '../../store';
-import { Typography } from '@mui/material';
+import { Grid, Skeleton, Typography } from '@mui/material';
 import { makeStyles } from '@mui/styles';
+import { getUser } from '../../store/auth/selectors';
+import { useHistory } from 'react-router-dom';
+import { LOGIN_ROUTE } from '../../routing/routes';
 
 const useStyles = makeStyles(() => ({
-  container: {
+  baseContainer: {
     display: 'flex',
     flexDirection: 'column',
     height: '100%',
+  },
+  headerContainer: {
+    marginLeft: 30,
+    marginRight: 30,
+    marginTop: 40,
   },
 }));
 
 const HomePage: React.FC = () => {
   const classes = useStyles();
   const dispatch = useDispatch();
+  const history = useHistory();
 
   useEffect(() => {
     dispatch(loadUserTasksForDay(new Date()));
@@ -26,9 +35,29 @@ const HomePage: React.FC = () => {
     getUserTaskListForDay(state, new Date())
   );
 
+  const user = useSelector(getUser);
+  if (!user) {
+    history.push(LOGIN_ROUTE);
+    return <Skeleton />;
+  }
+
   return (
-    <div className={classes.container}>
-      <Typography variant="h5">Your Tasks</Typography>
+    <div className={classes.baseContainer}>
+      <div className={classes.headerContainer}>
+        <Grid container direction="column" spacing={2}>
+          <Grid item container direction="column">
+            <Grid item>
+              <Typography variant="h4">Hello,</Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="h4">{user.displayName}</Typography>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Typography variant="h5">Your Tasks</Typography>
+          </Grid>
+        </Grid>
+      </div>
       <UserTaskCarousel userTaskList={userTaskList} />
     </div>
   );
