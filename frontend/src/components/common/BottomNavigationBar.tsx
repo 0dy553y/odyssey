f96344cab2e0df52f18b5d8cad3d5bb7be8e359a
Ian Yong <ianyongyc@gmail.com>
2021-10-16 16:19:41 +0800

    Remove redundant root element for bottom navigation bar

--- frontend/src/components/common/BottomNavigationBar.tsx
+++ frontend/src/components/common/BottomNavigationBar.tsx
@@ -17,43 +17,41 @@ const BottomNavigationBar: React.FC = () => {
   const location = useLocation();
 
   return (
-    <>
-      <Paper elevation={0}>
-        <BottomNavigation
-          showLabels
-          value={getFirstPathSegment(location.pathname)}
-        >
-          <BottomNavigationAction
-            label="Home"
-            icon={<HomeOutlinedIcon />}
-            value={HOME_ROUTE}
-            component={Link}
-            to={HOME_ROUTE}
-          />
-          <BottomNavigationAction
-            label="Explore"
-            icon={<SearchOutlinedIcon />}
-            value={EXPLORE_ROUTE}
-            component={Link}
-            to={EXPLORE_ROUTE}
-          />
-          <BottomNavigationAction
-            label="Feed"
-            icon={<PeopleOutlineIcon />}
-            value={FEED_ROUTE}
-            component={Link}
-            to={FEED_ROUTE}
-          />
-          <BottomNavigationAction
-            label="Profile"
-            icon={<AccountCircleOutlinedIcon />}
-            value={PROFILE_ROUTE}
-            component={Link}
-            to={PROFILE_ROUTE}
-          />
-        </BottomNavigation>
-      </Paper>
-    </>
+    <Paper elevation={0}>
+      <BottomNavigation
+        showLabels
+        value={getFirstPathSegment(location.pathname)}
+      >
+        <BottomNavigationAction
+          label="Home"
+          icon={<HomeOutlinedIcon />}
+          value={HOME_ROUTE}
+          component={Link}
+          to={HOME_ROUTE}
+        />
+        <BottomNavigationAction
+          label="Explore"
+          icon={<SearchOutlinedIcon />}
+          value={EXPLORE_ROUTE}
+          component={Link}
+          to={EXPLORE_ROUTE}
+        />
+        <BottomNavigationAction
+          label="Feed"
+          icon={<PeopleOutlineIcon />}
+          value={FEED_ROUTE}
+          component={Link}
+          to={FEED_ROUTE}
+        />
+        <BottomNavigationAction
+          label="Profile"
+          icon={<AccountCircleOutlinedIcon />}
+          value={PROFILE_ROUTE}
+          component={Link}
+          to={PROFILE_ROUTE}
+        />
+      </BottomNavigation>
+    </Paper>
   );
 };
 
