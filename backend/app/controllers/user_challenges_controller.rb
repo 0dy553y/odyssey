120e741f4ce72375d851548a723f7bf86999f95c
Sebastian Toh <sebas.tsj.98@gmail.com>
2021-10-16 21:47:57 +0800

    Send schedule to frontend

--- backend/app/controllers/user_challenges_controller.rb
+++ backend/app/controllers/user_challenges_controller.rb
@@ -3,8 +3,9 @@
 class UserChallengesController < ApplicationController
   def ongoing_user_challenge
     # looks for ongoing attempt
-    @user_challenge = current_user.user_challenges.find_by(challenge_id: params.require(:challenge_id),
-                                                           completed_at: nil, forfeited_at: nil)
+    @user_challenge = current_user.user_challenges.includes(:schedule).
+      find_by(challenge_id: params.require(:challenge_id),
+              completed_at: nil, forfeited_at: nil)
 
     return render 'layouts/empty', status: :ok if @user_challenge.nil?
 
--- frontend/src/types/userchallenge.ts
+++ frontend/src/types/userchallenge.ts
@@ -1,7 +1,9 @@
+import { Schedule } from './challenges';
 import { UserTaskListData } from './usertasks';
 
 export interface UserChallengeData {
   enrolledDate: Date;
   percentCompleted: number;
   userTasks: UserTaskListData[];
+  schedule: Schedule;
 }
