f72741f88415ef421d46481c4db66c9c0c88e7d4
Sebastian Toh <sebas.tsj.98@gmail.com>
2021-10-16 16:31:32 +0800

    Remove tasks_for_challenge in backend

--- backend/app/controllers/user_tasks_controller.rb
+++ backend/app/controllers/user_tasks_controller.rb
@@ -4,14 +4,4 @@ class UserTasksController < ApplicationController
   def tasks_for_day
     @user_tasks = current_user.user_tasks.where(scheduled_for: params.require(:date).to_date.all_day)
   end
-
-  def tasks_for_challenge
-    # looks for ongoing attempt
-    @user_challenge = current_user.user_challenges.find_by(challenge_id: params.require(:challenge_id),
-                                                           completed_at: nil, forfeited_at: nil)
-
-    unless @user_challenge.nil?
-      @user_tasks = current_user.user_tasks.includes(:task).where(user_challenge_id: @user_challenge.id)
-    end
-  end
 end
--- backend/app/views/user_tasks/tasks_for_challenge.json.jbuilder
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-json.data @user_tasks do |user_task|
-  json.id user_task.id
-  json.completedAt user_task.completed_at
-  json.index user_task.task.index
-end
