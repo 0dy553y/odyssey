59f1f0c44e845ee568ad246ad9ac4568242d0439
Ian Yong <ianyongyc@gmail.com>
2021-10-17 20:46:53 +0800

    Migrate from react-slick to Swiper

--- frontend/src/components/home/UserTaskCarousel.tsx
+++ frontend/src/components/home/UserTaskCarousel.tsx
@@ -1,36 +1,29 @@
 import React from 'react';
 import { UserTaskListData } from '../../types/usertasks';
 import UserTaskCard from './UserTaskCard';
-import Slider from 'react-slick';
-import { makeStyles } from '@mui/styles';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 import './UserTaskCarousel.scss';
-
-const useStyles = makeStyles(() => ({
-  sliderItem: {
-    height: '100%',
-    paddingTop: 20,
-    paddingBottom: 20,
-    paddingLeft: 10,
-    paddingRight: 10,
-  },
-}));
 
 interface Props {
   userTaskList: UserTaskListData[];
 }
 
 const UserTaskCarousel: React.FC<Props> = ({ userTaskList }: Props) => {
-  const classes = useStyles();
-
   return (
-    <Slider arrows={false} centerMode infinite={false} className="task-slider">
+    <Swiper
+      centeredSlides
+      slidesPerView={1.3}
+      spaceBetween={20}
+      slideToClickedSlide
+      className="task-slider"
+    >
       {userTaskList.map((userTask: UserTaskListData) => (
-        <div key={userTask.id} className={classes.sliderItem}>
+        <SwiperSlide key={userTask.id}>
           <UserTaskCard userTask={userTask} />
-        </div>
+        </SwiperSlide>
       ))}
-    </Slider>
+    </Swiper>
   );
 };
 
