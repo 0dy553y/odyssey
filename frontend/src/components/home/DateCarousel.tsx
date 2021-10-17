8f06a035e7acfd7333c7170384e8512609b458ca
Ian Yong <ianyongyc@gmail.com>
2021-10-17 16:25:33 +0800

    Add infinite date scrolling

--- frontend/src/components/home/DateCarousel.tsx
+++ frontend/src/components/home/DateCarousel.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { getNeighbouringDates } from '../../utils/date';
 import DateItem from './DateItem';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperClass from 'swiper/types/swiper-class';
 
 import './DateCarousel.scss';
 
@@ -11,25 +12,65 @@ interface Props {
 
 const DateCarousel: React.FC<Props> = ({ setDate }: Props) => {
   const dateRange = 8;
+  const previousIndex = dateRange;
   const [dates, setDates] = useState(
     getNeighbouringDates(new Date(), dateRange)
   );
 
+  const shiftDatesLeft = (by: number) => {
+    if (by <= 0) {
+      return;
+    }
+    const newDates = dates.slice(0, -by);
+    for (let i = 0; i < by; i++) {
+      const newDate = new Date(newDates[0]);
+      newDate.setDate(newDate.getDate() - 1);
+      newDates.unshift(newDate);
+    }
+    setDates(newDates);
+  };
+
+  const shiftDatesRight = (by: number) => {
+    if (by <= 0) {
+      return;
+    }
+    const newDates = dates.slice(by);
+    for (let i = 0; i < by; i++) {
+      const newDate = new Date(newDates[newDates.length - 1]);
+      newDate.setDate(newDate.getDate() + 1);
+      newDates.push(newDate);
+    }
+    setDates(newDates);
+  };
+
+  const handleTransitionEnd = (swiper: SwiperClass) => {
+    setDate(dates[swiper.activeIndex]);
+    const difference = swiper.activeIndex - previousIndex;
+    if (previousIndex > swiper.activeIndex) {
+      shiftDatesLeft(-difference);
+    } else if (previousIndex < swiper.activeIndex) {
+      shiftDatesRight(difference);
+    }
+    swiper.slideTo(dateRange, 0, false);
+  };
+
   return (
-    <Swiper
-      centeredSlides
-      initialSlide={dateRange}
-      slidesPerView={7}
-      watchSlidesProgress
-      className="date-slider"
-      onRealIndexChange={(swiper) => setDate(dates[swiper.activeIndex])}
-    >
-      {dates.map((date: Date) => (
-        <SwiperSlide key={date.toISOString()}>
-          <DateItem date={date} />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <>
+      <Swiper
+        centeredSlides
+        initialSlide={previousIndex}
+        slidesPerView={7}
+        watchSlidesProgress
+        className="date-slider"
+        onTransitionEnd={handleTransitionEnd}
+      >
+        {dates.map((date: Date) => (
+          <SwiperSlide key={date.toISOString()}>
+            <DateItem date={date} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
   );
 };
 
