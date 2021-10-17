8bc45c18311f52d0ceebf0b9a208e66ea1626b76
ming-00 <e0470728@u.nus.edu>
2021-10-17 20:11:00 +0800

    Fetch completed challenges in completed challenges page

--- frontend/src/pages/completedChallenges/CompletedChallengesPage.tsx
+++ frontend/src/pages/completedChallenges/CompletedChallengesPage.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ChevronLeft from '@mui/icons-material/ChevronLeft';
 import { AppBar, Box, IconButton, Toolbar, Typography } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { loadAllCompletedUserChallenges } from 'store/userchallenges/operations';
 import { useHistory } from 'react-router-dom';
+import { loadAllChallenges } from 'store/challenges/operations';
+import { getAllCompletedUserChallenges } from 'store/userchallenges/selectors';
+import { RootState } from 'store';
+import { getChallengeList } from 'store/challenges/selectors';
 
 const CompletedChallengesPage: React.FC = () => {
+  const dispatch = useDispatch();
   const history = useHistory();
+
+  useEffect(() => {
+    dispatch(loadAllCompletedUserChallenges());
+    dispatch(loadAllChallenges());
+  }, []);
+
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const completedChallenges = useSelector((state: RootState) =>
+    getAllCompletedUserChallenges(state)
+  )!;
+
+  console.log(completedChallenges);
+
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const parentChallenges = useSelector((state: RootState) =>
+    getChallengeList(state)
+  )!;
+
   return (
     <Box sx={{ padding: '2em 1.5em 0 1.5em' }}>
       <AppBar position="static">
