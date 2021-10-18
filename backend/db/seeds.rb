7314174ada9b4afca0c06cee859680816c2ad91e
Ian Yong <ianyongyc@gmail.com>
2021-10-18 22:51:46 +0800

    Update seeds for user testing

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -10,58 +10,53 @@
 
 user_odyssey = User.create(username: 'Odyssey', display_name: 'Odyssey', password: 'password')
 
-user_unclesoo = User.create(username: 'unclesoo', display_name: 'Uncle Soo', password: 'password')
+User.create(username: 'unclesoo', display_name: 'Uncle Soo', password: 'password')
 
 category_exercise = Category.create(title: 'Exercise')
 category_habits = Category.create(title: 'Habits')
 
-challenge_running = Challenge.create(category_id: category_exercise.id, name: 'Couch to 5k',
-                                     description: 'Couch to 5K is a running plan for absolute beginners.
-                                     It was developed by a new runner, Josh Clark,
-                                     who wanted to help his 50-something mum get off the couch and start running, too.',
-                                     schedule: '3 times a week, alternate days',
-                                     duration: 30,
+challenge_gratitude_desc = <<~DESCRIPTION
+  While it’s important to analyze and learn from bad events, sometimes we can think too much about what goes wrong and
+  not enough about what goes right in our lives. A gratitude journal encourages ourselves to pay attention to the good
+  things in life we might otherwise take for granted. In that way, we start to become more attuned to the everyday
+  sources of pleasure around us—and the emotional tone of our life can shift in profound ways.
+DESCRIPTION
+challenge_gratitude = Challenge.create(category_id: category_habits.id, name: 'Gratitude Journaling',
+                                       description: challenge_gratitude_desc,
+                                       duration: 3,
+                                       creator_id: user_odyssey.id,
+                                       color: 0)
+Task.create(challenge_id: challenge_gratitude.id,
+            name: 'Day 1',
+            description: 'Write down three things that happened today that you are grateful for.',
+            index: 0)
+Task.create(challenge_id: challenge_gratitude.id,
+            name: 'Day 2',
+            description: 'Write down two people you are grateful for, and why.',
+            index: 1)
+Task.create(challenge_id: challenge_gratitude.id,
+            name: 'Day 3',
+            description: 'Don’t complain for a day, and write about your experience.',
+            index: 2)
+
+challenge_walking_desc = <<~DESCRIPTION
+  Walking is a great way to improve or maintain your overall health. Just 30 minutes every day can increase
+  cardiovascular fitness, strengthen bones, reduce excess body fat, and boost muscle power and endurance.
+DESCRIPTION
+challenge_walking = Challenge.create(category_id: category_exercise.id, name: 'Gratitude Journaling',
+                                     description: challenge_walking_desc,
+                                     duration: 3,
                                      creator_id: user_odyssey.id,
-                                     color: 0)
-user_challenge_running = UserChallenge.new(user_id: user_unclesoo.id, challenge_id: challenge_running.id,
-                                           started_at: 1.week.ago)
-user_challenge_running.schedule = Schedule.new(saturday: true, sunday: true)
-user_challenge_running.save!
-
-challenge_drinking = Challenge.create(category_id: category_habits.id, name: 'Water Drinking',
-                                      description: 'Get hydrated with Jialin', schedule: 'Daily', duration: 36_464_572,
-                                      creator_id: user_odyssey.id, color: 1)
-user_challenge_drinking = UserChallenge.new(user_id: user_unclesoo.id, challenge_id: challenge_drinking.id,
-                                            started_at: 2.weeks.ago)
-user_challenge_drinking.schedule = Schedule.new(monday: true, tuesday: true, wednesday: true,
-                                                thursday: true, friday: true)
-user_challenge_drinking.save!
-
-challenge_journalling = Challenge.new(category_id: category_habits.id, name: 'Journalling',
-                                      description: 'Dear Diary', schedule: 'Daily', duration: 30,
-                                      creator_id: user_odyssey.id, color: 0)
-challenge_journalling.save!
-
-30.times do |i|
-  description = "Welcome to the #{(i + 1).ordinalize} day of Couch to 5k. Today, you will run till you drop. Okay, go!"
-  task = Task.create(challenge_id: challenge_running.id, name: "Day #{i + 1}: Warming Up",
-                     description: description, index: i)
-  UserTask.create(user_id: user_unclesoo.id, user_challenge_id: user_challenge_running.id, task_id: task.id,
-                  completed_at: i < 1.week.in_days ? 1.week.ago + i.days : nil,
-                  scheduled_for: 1.week.ago + i.days)
-end
-
-30.times do |i|
-  description = "Jialin drank #{i + 1} cups of water today. Can you?"
-  task = Task.create(challenge_id: challenge_drinking.id, name: "Day #{i + 1}: Bling Blong",
-                     description: description, index: i)
-  UserTask.create(user_id: user_unclesoo.id, user_challenge_id: user_challenge_drinking.id, task_id: task.id,
-                  completed_at: i < 2.weeks.in_days ? 2.weeks.ago + i.days : nil,
-                  scheduled_for: 2.weeks.ago + i.days)
-end
-
-30.times do |i|
-  description = "Write #{(i + 1).ordinalize} pages today"
-  Task.create(challenge_id: challenge_journalling.id, name: "Day #{i + 1}: What's up",
-              description: description, index: i)
-end
+                                     color: 1)
+Task.create(challenge_id: challenge_walking.id,
+            name: 'Day 1',
+            description: 'Go for a walk for at least 5 min today.',
+            index: 0)
+Task.create(challenge_id: challenge_walking.id,
+            name: 'Day 2',
+            description: 'Go for a walk for at least 10 min today.',
+            index: 1)
+Task.create(challenge_id: challenge_walking.id,
+            name: 'Day 3',
+            description: 'Go for a walk for at least 15 min today.',
+            index: 2)
