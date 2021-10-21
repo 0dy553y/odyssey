4401ea9c4b2e76f6120488e35b2f0ed5d82ac043
jialin7878 <59867654+jialin7878@users.noreply.github.com>
2021-10-21 21:42:50 +0800

    Add DemoMapTheme

---
+++ frontend/src/pages/map/DemoMapTheme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#ff6ad5',
+    },
+    secondary: {
+      main: '#8690ff',
+    },
+  },
+});
+
+export default theme;
