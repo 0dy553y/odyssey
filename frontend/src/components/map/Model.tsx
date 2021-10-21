abf8f4706ed9737e559556b09293c373ad0c611c
jialin7878 <59867654+jialin7878@users.noreply.github.com>
2021-10-21 21:39:44 +0800

    Add component Model

---
+++ frontend/src/components/map/Model.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useLoader } from '@react-three/fiber';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader';
+import { MapComponent } from './MapComponent';
+
+interface ModelProps {
+  fileName: string;
+  scale?: number;
+}
+
+const Model: React.FC<ModelProps> = ({ fileName, scale = 1 }) => {
+  const materials = useLoader(MTLLoader, `/${fileName}.mtl`);
+  const obj = useLoader(OBJLoader, `/${fileName}.obj`, (loader) => {
+    materials.preload();
+    loader.setMaterials(materials);
+  });
+  return <primitive object={obj} scale={scale} />;
+};
+
+export default MapComponent(Model);
