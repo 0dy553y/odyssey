3604f84d0c660e4b7dbab90f191fb44f733458fe
Sebastian Toh <sebas.tsj.98@gmail.com>
2021-10-22 22:27:57 +0800

    Add posts types

---
+++ frontend/src/types/posts.ts
@@ -0,0 +1,22 @@
+import { UserData } from './auth';
+
+export interface PostsListData {
+  id: number;
+  body: string;
+  creator: Omit<UserData, 'registrationDate'>;
+  reactions: ReactionListData[];
+}
+
+export interface ReactionListData {
+  emoji: ReactionEmoji;
+  creator: Omit<UserData, 'registrationDate'>;
+}
+
+enum ReactionEmoji {
+  Smiley = 0,
+  Poop = 1,
+  PartyPopper = 2,
+  SparklingHeart = 3,
+  Crying = 4,
+  MoonFace = 5,
+}
