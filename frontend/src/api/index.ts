856ae331643f8089bd1b222bfc3b0a086e0f3b6b
Ian Yong <ianyongyc@gmail.com>
2021-10-22 16:20:37 +0800

    Add friend & friend request APIs to exported API object

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -4,11 +4,15 @@ import ChallengesAPI from './challenges';
 import TasksAPI from './tasks';
 import UserChallengesAPI from './userchallenge';
 import UserTasksAPI from './usertasks';
+import FriendsAPI from './friends';
+import FriendRequestsAPI from './friendrequests';
 
 const api = Object.freeze({
   auth: new AuthAPI(),
   categories: new CategoriesAPI(),
   challenges: new ChallengesAPI(),
+  friends: new FriendsAPI(),
+  friendRequests: new FriendRequestsAPI(),
   tasks: new TasksAPI(),
   userTasks: new UserTasksAPI(),
   userChallenges: new UserChallengesAPI(),
