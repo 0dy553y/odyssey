b6f1039e3bd1034bed27487bee4ffb2659e60785
Ian Yong <ianyongyc@gmail.com>
2021-10-22 19:04:16 +0800

    Mirror database validation rule in friendship model

--- backend/app/models/friendship.rb
+++ backend/app/models/friendship.rb
@@ -3,4 +3,6 @@
 class Friendship < ApplicationRecord
   belongs_to :first_user, class_name: 'User'
   belongs_to :second_user, class_name: 'User'
+  validates :first_user_id, numericality: { less_than: :second_user_id,
+                                            message: 'must have a smaller ID than second user' }
 end
