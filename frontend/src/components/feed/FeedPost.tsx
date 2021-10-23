010d8ac8a138390d8676f98cf47437bf1a829583
Sebastian Toh <sebas.tsj.98@gmail.com>
2021-10-23 16:07:31 +0800

    Display reactions in FeedPost

--- frontend/src/components/feed/FeedPost.tsx
+++ frontend/src/components/feed/FeedPost.tsx
@@ -1,31 +1,87 @@
-import * as React from 'react';
+import React from 'react';
 import {
   ListItem,
   ListItemText,
   ListItemAvatar,
   Tooltip,
   Typography,
+  Grid,
 } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import UserAvatar from 'components/common/userAvatar';
-import { PostListData } from 'types/posts';
+import { PostListData, ReactionEmoji, ReactionListData } from 'types/posts';
 import { displayDateWithTimestamp, displayUsername } from 'utils/formatting';
 import { getDateFromNowString } from 'utils/date';
+import { ReactionChip } from './ReactionChip';
 
 interface FeedPostProps {
   post: PostListData;
+  currentUserId: number;
 }
 
 const useStyles = makeStyles(() => ({
   subtitle: {
     color: '#A5A5A5',
   },
+  emojiChip: {
+    margin: 2,
+  },
 }));
 
-export const FeedPost: React.FC<FeedPostProps> = ({ post }) => {
+const getAggregatedReactions = (
+  reactions: ReactionListData[],
+  currentUserId: number
+): {
+  emoji: ReactionEmoji;
+  count: number;
+  hasReacted: boolean;
+}[] => {
+  const emojiMap: Map<ReactionEmoji, { count: number; hasReacted: boolean }> =
+    new Map();
+
+  reactions.forEach((reaction) => {
+    const prevVal = emojiMap.get(reaction.emoji) ?? {
+      count: 0,
+      hasReacted: false,
+    };
+    const updatedVal = {
+      count: prevVal.count + 1,
+      hasReacted: prevVal.hasReacted || currentUserId == reaction.creator.id,
+    };
+    emojiMap.set(reaction.emoji, updatedVal);
+  });
+
+  const aggregatedData: {
+    emoji: ReactionEmoji;
+    count: number;
+    hasReacted: boolean;
+  }[] = [];
+
+  Object.values(ReactionEmoji).forEach((emoji) => {
+    const data = emojiMap.get(emoji as ReactionEmoji);
+
+    if (!data) {
+      return;
+    }
+
+    aggregatedData.push({
+      emoji: emoji as ReactionEmoji,
+      count: data.count,
+      hasReacted: data.hasReacted,
+    });
+  });
+
+  return aggregatedData;
+};
+
+export const FeedPost: React.FC<FeedPostProps> = ({ post, currentUserId }) => {
   const classes = useStyles();
 
   const creator = post.creator;
+  const aggregatedReactionData = getAggregatedReactions(
+    post.reactions,
+    currentUserId
+  );
 
   return (
     <ListItem alignItems="flex-start">
@@ -40,22 +96,38 @@ export const FeedPost: React.FC<FeedPostProps> = ({ post }) => {
         primary={creator.displayName ?? displayUsername(creator.username)}
         secondary={
           <>
-            {post.body}
-            <br />
-            <Tooltip
-              arrow
-              title={displayDateWithTimestamp(post.createdAt)}
-              leaveTouchDelay={1500}
-              enterTouchDelay={50}
-            >
-              <Typography
-                component="span"
-                variant="subtitle2"
-                className={classes.subtitle}
-              >
-                {getDateFromNowString(post.createdAt)}
-              </Typography>
-            </Tooltip>
+            <Grid container>
+              <Grid item>{post.body}</Grid>
+              <Grid item xs={12}>
+                <Tooltip
+                  arrow
+                  title={displayDateWithTimestamp(post.createdAt)}
+                  leaveTouchDelay={1500}
+                  enterTouchDelay={50}
+                >
+                  <Typography
+                    component="span"
+                    variant="subtitle2"
+                    className={classes.subtitle}
+                  >
+                    {getDateFromNowString(post.createdAt)}
+                  </Typography>
+                </Tooltip>
+              </Grid>
+
+              {aggregatedReactionData.map((data) => {
+                return (
+                  <Grid item key={data.emoji} className={classes.emojiChip}>
+                    <ReactionChip
+                      reaction={data.emoji}
+                      count={data.count}
+                      hasReacted={data.hasReacted}
+                      onClick={() => console.log('HELLO')}
+                    />
+                  </Grid>
+                );
+              })}
+            </Grid>
           </>
         }
       />
--- frontend/src/pages/feed/FeedPage.tsx
+++ frontend/src/pages/feed/FeedPage.tsx
@@ -1,11 +1,92 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
+import { FeedPost } from 'components/feed/FeedPost';
+import { subDays } from 'date-fns/esm';
+import { ReactionEmoji } from 'types/posts';
 
 const FeedPage: React.FC = () => {
+  const mockPost = {
+    id: 1,
+    body: `After a very LONG journey, I finally have reached the PINNACLE and completed the 3-day
+    'Walking' challenge!!! I want to take time to thank my friends, family, and colleagues!
+    Thank you for continuing to believe in me and pushing me towards success.
+    Never ever give up folks. If you want something, go get it! Perseverance, GRIT,
+    and passion go a long way.
+    `,
+    createdAt: subDays(new Date(), 7),
+    creator: {
+      id: 1,
+      username: 'bobby',
+    },
+    reactions: [
+      {
+        emoji: ReactionEmoji.Crying,
+        creator: {
+          id: 1,
+          username: 'bobby',
+        },
+      },
+      {
+        emoji: ReactionEmoji.Crying,
+        creator: {
+          id: 2,
+          username: 'robby',
+        },
+      },
+      {
+        emoji: ReactionEmoji.Crying,
+        creator: {
+          id: 3,
+          username: 'poppy',
+        },
+      },
+      {
+        emoji: ReactionEmoji.Smiley,
+        creator: {
+          id: 1,
+          username: 'bobby',
+        },
+      },
+      {
+        emoji: ReactionEmoji.MoonFace,
+        creator: {
+          id: 1,
+          username: 'bobby',
+        },
+      },
+      {
+        emoji: ReactionEmoji.PartyPopper,
+        creator: {
+          id: 1,
+          username: 'bobby',
+        },
+      },
+      {
+        emoji: ReactionEmoji.Poop,
+        creator: {
+          id: 1,
+          username: 'bobby',
+        },
+      },
+      {
+        emoji: ReactionEmoji.SparklingHeart,
+        creator: {
+          id: 1,
+          username: 'bobby',
+        },
+      },
+    ],
+  };
+
   return (
     <>
       <Typography variant="h5">Upcoming :-)</Typography>
       <img src="https://i.redd.it/ox49yfg9vn461.jpg" style={{ width: '80%' }} />
+
+      <FeedPost post={mockPost} currentUserId={1} />
+      <FeedPost post={mockPost} currentUserId={1} />
+      <FeedPost post={mockPost} currentUserId={1} />
+      <FeedPost post={mockPost} currentUserId={1} />
     </>
   );
 };
