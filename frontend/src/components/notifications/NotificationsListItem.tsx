afea9fb1c16e328609e84e42e968a1e53cb8c6cb
Ian Yong <ianyongyc@gmail.com>
2021-10-25 16:27:17 +0800

    Update friend request message

--- frontend/src/components/notifications/NotificationsListItem.tsx
+++ frontend/src/components/notifications/NotificationsListItem.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import { FriendRequestListData } from '../../types/friendrequests';
-import { ListItem, ListItemAvatar, ListItemText } from '@mui/material';
+import {
+  Grid,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Tooltip,
+  Typography,
+} from '@mui/material';
 import UserAvatar from '../common/userAvatar';
-import { displayUsername } from '../../utils/formatting';
+import {
+  displayDateWithTimestamp,
+  displayUsername,
+} from '../../utils/formatting';
+import { getDateFromNowString } from '../../utils/date';
+import { ReactionChip } from '../feed/ReactionChip';
+import { ReactionPicker } from '../feed/ReactionPicker';
 
 interface Props {
   friendRequest: FriendRequestListData;
@@ -20,10 +33,34 @@ const NotificationsListItem: React.FC<Props> = ({ friendRequest }: Props) => {
           displayName={sender.displayName}
         />
       </ListItemAvatar>
-      <ListItemText
-        primary={sender.displayName ?? displayUsername(sender.username)}
-        secondary={sender.displayName && displayUsername(sender.username)}
-      />
+      <Grid container alignItems="center">
+        <ListItemText
+          primary={
+            sender.displayName
+              ? `${sender.displayName} (${displayUsername(sender.username)})`
+              : displayUsername(sender.username)
+          }
+        />
+        <Grid item xs={12}>
+          sent you a friend request!
+        </Grid>
+        {/*<Grid item xs={12}>*/}
+        {/*  <Tooltip*/}
+        {/*    arrow*/}
+        {/*    title={displayDateWithTimestamp(post.createdAt)}*/}
+        {/*    leaveTouchDelay={1500}*/}
+        {/*    enterTouchDelay={50}*/}
+        {/*  >*/}
+        {/*    <Typography*/}
+        {/*      component="span"*/}
+        {/*      variant="subtitle2"*/}
+        {/*      className={classes.subtitle}*/}
+        {/*    >*/}
+        {/*      {getDateFromNowString(post.createdAt)}*/}
+        {/*    </Typography>*/}
+        {/*  </Tooltip>*/}
+        {/*</Grid>*/}
+      </Grid>
     </ListItem>
   );
 };
