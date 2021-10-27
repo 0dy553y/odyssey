a06160998f9709144bc738f5555ba347581f76d3
Sebastian Toh <sebas.tsj.98@gmail.com>
2021-10-27 20:25:16 +0800

    Add validations to form

--- frontend/src/components/feed/CreatePostModal.tsx
+++ frontend/src/components/feed/CreatePostModal.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import {
+  Box,
   Button,
   Dialog,
   DialogTitle,
   DialogContent,
-  DialogContentText,
   DialogActions,
+  MenuItem,
   useMediaQuery,
   useTheme,
+  Theme,
   TextField,
 } from '@mui/material';
+import { useForm, Controller } from 'react-hook-form';
+import { makeStyles } from '@mui/styles';
 
 import './ReactionChip.scss';
 
@@ -19,13 +23,37 @@ interface CreatePostModalProps {
   onSubmit: () => void;
 }
 
+interface CreatePostFormState {
+  challengeId: number | string | null;
+  body: string;
+}
+
+const useStyles = makeStyles((theme: Theme) => ({
+  createPostForm: {
+    '&>:not(:last-child)': {
+      marginBottom: '1em',
+    },
+  },
+}));
+
 export const CreatePostModal: React.FC<CreatePostModalProps> = ({
   isOpen,
   onClose,
   onSubmit,
 }) => {
   const theme = useTheme();
+  const classes = useStyles(theme);
   const shouldFullScreen = useMediaQuery(theme.breakpoints.only('xs'));
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<CreatePostFormState>();
+
+  const onFormSubmit = handleSubmit((data: CreatePostFormState) => {
+    console.log(data);
+    onSubmit();
+  });
 
   return (
     <Dialog
@@ -35,26 +63,65 @@ export const CreatePostModal: React.FC<CreatePostModalProps> = ({
       fullWidth
       maxWidth="sm"
     >
-      <DialogTitle>Create Post</DialogTitle>
-      <DialogContent>
-        <TextField
-          multiline
-          autoFocus
-          rows={5}
-          fullWidth
-          variant="standard"
-          label="Post Body"
-          defaultValue="TODO"
-        />
-      </DialogContent>
-      <DialogActions>
-        <Button autoFocus onClick={onClose} variant="outlined">
-          Close
-        </Button>
-        <Button autoFocus onClick={onSubmit} variant="contained">
-          Post
-        </Button>
-      </DialogActions>
+      <Box component="form" noValidate onSubmit={onFormSubmit}>
+        <DialogTitle>Create Post</DialogTitle>
+
+        <DialogContent className={classes.createPostForm}>
+          <Controller
+            name="challengeId"
+            control={control}
+            rules={{ required: 'Please select a challenge' }}
+            defaultValue=""
+            render={({ field }) => (
+              <TextField
+                {...field}
+                required
+                fullWidth
+                autoFocus
+                variant="standard"
+                label="Challenge"
+                error={!!errors.challengeId}
+                helperText={errors.challengeId?.message}
+                select
+              >
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </TextField>
+            )}
+          />
+
+          <Controller
+            name="body"
+            control={control}
+            defaultValue=""
+            rules={{ required: 'Post cannot be empty' }}
+            render={({ field }) => (
+              <TextField
+                {...field}
+                required
+                fullWidth
+                autoFocus
+                variant="standard"
+                label="Text"
+                error={!!errors.body}
+                helperText={errors.body?.message}
+                multiline
+                rows={5}
+              />
+            )}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={onClose} variant="outlined">
+            Close
+          </Button>
+
+          <Button autoFocus variant="contained" type="submit">
+            Post
+          </Button>
+        </DialogActions>
+      </Box>
     </Dialog>
   );
 };
