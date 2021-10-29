6257b17f012805dc55eca3ea594a5b4aa0dbf018
Sebastian Toh <sebas.tsj.98@gmail.com>
2021-10-29 14:30:48 +0800

    Add navigation to user profile on avatar click

--- frontend/src/components/common/userAvatar/UserAvatar.tsx
+++ frontend/src/components/common/userAvatar/UserAvatar.tsx
@@ -2,6 +2,10 @@ import * as React from 'react';
 import { Avatar } from '@mui/material';
 import { DataUrl } from 'types/auth';
 import { stringAvatar } from 'utils/avatar';
+import { useHistory } from 'react-router-dom';
+import { PROFILE_ROUTE } from 'routing/routes';
+import { getUser } from 'store/auth/selectors';
+import { useSelector } from 'react-redux';
 
 interface UserAvatarProps {
   src: DataUrl | undefined;
@@ -16,12 +20,22 @@ const UserAvatar: React.FC<UserAvatarProps> = ({
   displayName,
   className,
 }) => {
+  const history = useHistory();
+  const user = useSelector(getUser);
+
   return (
     <Avatar
       style={{ marginBottom: '0.5em' }}
       className={className}
       src={src}
       {...stringAvatar(displayName ?? username)}
+      onClick={() => {
+        if (user?.username === username) {
+          history.push(`${PROFILE_ROUTE}`);
+          return;
+        }
+        history.push(`${PROFILE_ROUTE}/${username}`);
+      }}
     />
   );
 };
