1e4e4d66db17b6625f4278da77e8e50b3bd3d293
jialin7878 <E0425498@u.nus.edu>
2021-10-30 17:12:54 +0800

    Fix CSS breakage

--- frontend/src/pages/maps/MapsPage.tsx
+++ frontend/src/pages/maps/MapsPage.tsx
@@ -2,14 +2,16 @@ import React, { useEffect } from 'react';
 import { batch, useDispatch, useSelector } from 'react-redux';
 import { loadFriendsOnSameChallenges } from 'store/challenges/operations';
 import { getChallengeMaps } from 'store/challenges/selectors';
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import MapCarousel from '../../components/map/MapCarousel';
 import { RootState } from 'store';
-import { default as Appbar } from '../../components/common/Appbar';
+import { AppBar, IconButton, Toolbar } from '@mui/material';
+import { ReactComponent as BackArrow } from 'assets/icons/arrow-left.svg';
 import { ChallengeMapData } from 'types/challenges';
 
 const MapsPage: React.FC = () => {
   const { challengeId } = useParams<{ challengeId: string }>();
+  const history = useHistory();
   const dispatch = useDispatch();
   useEffect(() => {
     batch(() => {
@@ -29,7 +31,19 @@ const MapsPage: React.FC = () => {
 
   return (
     <>
-      <Appbar />
+      <AppBar position="absolute">
+        <Toolbar>
+          <div
+            onClick={() => {
+              history.goBack();
+            }}
+          >
+            <IconButton edge="start" sx={{ color: 'white', padding: '1em' }}>
+              <BackArrow height="1.5em" width="1.5em" />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
       <MapCarousel
         maps={challengeMaps}
         initialIndex={challengeIdToSwiperIndexMap[challengeId]}
