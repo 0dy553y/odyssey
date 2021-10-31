1ab8d304de1b720dbd46f77f23cf0d2d46c82955
Ian Yong <ianyongyc@gmail.com>
2021-11-01 01:18:12 +0800

    Add BASE_CLIENT_URL environment variable

--- deployment/env.template.js
+++ deployment/env.template.js
@@ -2,6 +2,7 @@
 // Placeholders here will be replaced with values from the environment at container startup
 
 window.env = {
+  BASE_CLIENT_URL: '$BASE_CLIENT_URL',
   BASE_SERVER_URL: '$BASE_SERVER_URL',
   GOOGLE_ANALYTICS_ID: '$GOOGLE_ANALYTICS_ID',
 };
