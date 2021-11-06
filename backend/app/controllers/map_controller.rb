4a94d1e9791d73388a83435194424673f9e2cd7e
jialin7878 <E0425498@u.nus.edu>
2021-11-07 00:43:11 +0800

    Rubocop

--- backend/app/controllers/map_controller.rb
+++ backend/app/controllers/map_controller.rb
@@ -12,6 +12,7 @@ class MapController < ApplicationController
   end
 
   private
+
   def construct_user_challenge_map_data(user_challenge)
     {
       challenge_id: user_challenge.challenge_id,
@@ -21,16 +22,16 @@ class MapController < ApplicationController
       num_tasks: user_challenge.challenge.tasks.count,
       current_task_num: user_challenge.user_tasks.where.not(completed_at: nil).count + 1,
       friends: current_user.friends.select do |f|
-                f.user_challenges.ongoing.where(challenge_id: user_challenge.challenge_id).first
-              end.map do |f|
-                {
-                  username: f.username,
-                  display_name: f.display_name,
-                  character: f.character,
-                  current_task_num: f.user_challenges.ongoing.where(challenge_id: user_challenge.challenge_id).first
+                 f.user_challenges.ongoing.where(challenge_id: user_challenge.challenge_id).first
+               end.map do |f|
+                 {
+                   username: f.username,
+                   display_name: f.display_name,
+                   character: f.character,
+                   current_task_num: f.user_challenges.ongoing.where(challenge_id: user_challenge.challenge_id).first
                                       .user_tasks.where.not(completed_at: nil).size + 1
-                }
-              end
+                 }
+               end
     }
   end
 end
