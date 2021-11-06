58ac80d42545de364b87a2af476ed4cb68dd3203
jialin7878 <E0425498@u.nus.edu>
2021-11-07 01:31:08 +0800

    Update completed map page to display map

--- frontend/src/pages/maps/CompletedMapPage.tsx
+++ frontend/src/pages/maps/CompletedMapPage.tsx
@@ -1,33 +1,56 @@
 import React, { useEffect } from 'react';
 import { batch, useDispatch, useSelector } from 'react-redux';
-import { loadAllOngoingChallengeMaps } from 'store/userchallenges/operations';
-import { getChallengeMaps } from 'store/userchallenges/selectors';
+import { loadChallengeMap } from 'store/userchallenges/operations';
+import { getChallengeMap } from 'store/userchallenges/selectors';
 import { useHistory, useParams } from 'react-router-dom';
-import MapCarousel from '../../components/map/MapCarousel';
 import { RootState } from 'store';
-import { AppBar, Box, IconButton, Toolbar } from '@mui/material';
+import {
+  AppBar,
+  Box,
+  IconButton,
+  Stack,
+  Toolbar,
+  Typography,
+} from '@mui/material';
 import { ReactComponent as BackArrow } from 'assets/icons/arrow-left.svg';
-import { UserChallengeMapData } from 'types/userchallenge';
+import { makeStyles } from '@mui/styles';
+import MapWrapper from 'components/map/mapTemplates/MapWrapper';
+
+const useStyles = makeStyles(() => ({
+  header: {
+    color: 'white',
+  },
+  name: {
+    position: 'absolute',
+    zIndex: 10,
+    marginLeft: 96,
+    marginTop: 24,
+  },
+  map: {
+    position: 'absolute',
+    height: '100vh',
+    width: '100%',
+  },
+  container: {
+    position: 'relative',
+  },
+}));
 
 const CompletedMapPage: React.FC = () => {
   const { challengeId } = useParams<{ challengeId: string }>();
   const history = useHistory();
   const dispatch = useDispatch();
+  const classes = useStyles();
   useEffect(() => {
     batch(() => {
-      dispatch(loadAllOngoingChallengeMaps());
+      dispatch(loadChallengeMap(Number(challengeId)));
     });
   }, []);
 
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  const challengeMaps = useSelector((state: RootState) =>
-    getChallengeMaps(state)
+  const mapData = useSelector((state: RootState) =>
+    getChallengeMap(state, Number(challengeId))
   )!;
-
-  const challengeIdToSwiperIndexMap: Record<string, number> = {};
-  challengeMaps.map((data: UserChallengeMapData, index: number) => {
-    challengeIdToSwiperIndexMap[data.challengeId] = index;
-  });
 
   return (
     <>
@@ -44,11 +67,17 @@ const CompletedMapPage: React.FC = () => {
           </div>
         </Toolbar>
       </AppBar>
-      <Box>
-        <MapCarousel
-          maps={challengeMaps}
-          initialIndex={challengeIdToSwiperIndexMap[challengeId]}
-        />
+      <Box sx={{ height: '100%' }}>
+        <div className={classes.container}>
+          <Stack className={classes.name}>
+            <Typography variant="h1" className={classes.header}>
+              {mapData.challengeName}
+            </Typography>
+          </Stack>
+          <div className={classes.map}>
+            <MapWrapper mapData={mapData} />
+          </div>
+        </div>
       </Box>
     </>
   );
