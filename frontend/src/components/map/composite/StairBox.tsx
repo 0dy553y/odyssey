18a43f08bc5a4f15936ffacb561b85186b8cb0ee
jialin7878 <E0425498@u.nus.edu>
2021-11-09 00:48:12 +0800

    Add HalfArch and StairBox

--- frontend/src/components/map/composite/StairBox.tsx
+++ frontend/src/components/map/composite/StairBox.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Direction } from 'types/map';
-import { Arch, Box, Stairs } from '..';
+import { Box, Stairs } from '..';
+import HalfArch from '../basic/HalfArch';
 import { Color, MapComponent } from '../basic/MapComponent';
 
 const StairBox: React.FC<Color> = ({ color }) => {
   return (
     <>
-      <Stairs color={color} position={[0, 0, 0]} />
-      <Box color={color} position={[0, 0, 1]} />
-      <Arch
+      <Stairs color={color} position={[0, -0.5, -1]} />
+      <Box color={color} position={[0, -0.5, 0]} />
+      <HalfArch
         color={color}
-        position={[0, 0, 2]}
-        direction={Direction.RIGHT}
+        position={[0, -0.5, 2]}
+        direction={Direction.LEFT}
         width={2}
       />
     </>
